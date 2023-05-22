72f431fc2097f41d22285fab80b1913a8c0a0402
Vitaly Sem <Vir2S@users.noreply.github.com>
2023-05-22 18:21:41 +0200

    Poke api (#3)
    * pokemon api endpoint added
    
    * pokemon api endpoint fixed
    
    * not used imports deleted
    
    * get_all_pokemons endpoint added
    
    * csrf_exempt decorator added
    
    * statuses added
    
    * delete pokemon added
    
    * black codestyle fixed
    
    * isort fixed

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,168 @@
-"""
-URL configuration for config project.
+import json
+from dataclasses import asdict, dataclass
+from datetime import datetime, timedelta
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+import requests
+from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
+
+def filter_by_keys(source: dict, keys: list[str]) -> dict:
+    filtered_data = {}
+
+    for key, value in source.items():
+        if key in keys:
+            filtered_data[key] = value
+
+    return filtered_data
+
+
+@dataclass
+class Pokemon:
+    id: int
+    name: str
+    height: int
+    weight: int
+    base_experience: int
+
+    @classmethod
+    def from_raw_data(cls, raw_data: dict) -> "Pokemon":
+        filtered_data = filter_by_keys(
+            raw_data,
+            cls.__dataclass_fields__.keys(),
+        )
+        return cls(**filtered_data)
+
+
+# ============================================
+# Simulate the CACHE
+# ============================================
+TTL = timedelta(seconds=5)
+POKEMONS: dict[str, list[Pokemon, datetime]] = {}
+
+
+def get_pokemon_from_api(name: str) -> Pokemon | None:
+    url = settings.POKEAPI_BASE_URL + f"/{name}"
+    response = requests.get(url)
+
+    if response.status_code == 404:
+        return None
+
+    raw_data = response.json()
+
+    return Pokemon.from_raw_data(raw_data)
+
+
+def _get_pokemon(name) -> Pokemon | None:
+    """
+    Take pokemon from the cache or
+    fetch it from the API and then save it to the cache.
+    """
+
+    if name in POKEMONS:
+        pokemon, created_at = POKEMONS[name]
+
+        if datetime.now() > created_at + TTL:
+            del POKEMONS[name]
+            return _get_pokemon(name)
+    else:
+        pokemon: Pokemon = get_pokemon_from_api(name)
+        if pokemon:
+            POKEMONS[name] = [pokemon, datetime.now()]
+        return pokemon
+    return None
+
+
+@csrf_exempt
+def get_delete_pokemon(request, name: str):
+    if request.method == "GET":
+        pokemon: Pokemon = _get_pokemon(name)
+
+        if pokemon:
+            return HttpResponse(
+                content_type="application/json",
+                content=json.dumps(asdict(pokemon)),
+            )
+
+        return HttpResponse(
+            content_type="application/json",
+            content=json.dumps({"error": "Pokemon not found"}),
+            status=404,
+        )
+
+    if request.method == "DELETE":
+        if name in POKEMONS:
+            del POKEMONS[name]
+            return HttpResponse(
+                content_type="application/json",
+                content=json.dumps({"message": f"Pokemon '{name}' removed from cache"}),
+                status=204,
+            )
+
+        return HttpResponse(
+            content_type="application/json",
+            content=json.dumps({"error": "Pokemon not found in cache"}),
+            status=404,
+        )
+
+    return HttpResponse(
+        content_type="application/json",
+        content=json.dumps({"error": "Method not allowed"}),
+        status=400,
+    )
+
+
+@csrf_exempt
+def get_pokemon_for_mobile(request, name: str):
+    if request.method == "GET":
+        pokemon: Pokemon = _get_pokemon(name)
+
+        if pokemon:
+            result = filter_by_keys(
+                asdict(pokemon),
+                ["id", "name", "base_experience"],
+            )
+
+            return HttpResponse(
+                content_type="application/json",
+                content=json.dumps(result),
+            )
+
+        return HttpResponse(
+            content_type="application/json",
+            content=json.dumps({"error": "Pokemon not found"}),
+            status=404,
+        )
+
+    return HttpResponse(
+        content_type="application/json",
+        content=json.dumps({"error": "Method not allowed"}),
+        status=400,
+    )
+
+
+@csrf_exempt
+def get_all_pokemons(request):
+    if request.method == "GET":
+        pokemons: list = [asdict(pokemon) for pokemon, _ in POKEMONS.values()]
+        return HttpResponse(
+            content_type="application/json",
+            content=json.dumps(pokemons),
+        )
+
+    return HttpResponse(
+        content_type="application/json",
+        content=json.dumps({"error": "Method not allowed"}),
+        status=400,
+    )
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/pokemons/", get_all_pokemons),
+    path("api/pokemon/<str:name>/", get_delete_pokemon),
+    path("api/pokemon/mobile/<str:name>/", get_pokemon_for_mobile),
 ]
