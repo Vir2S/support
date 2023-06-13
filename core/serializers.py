ec26144ff4fa2714a1cdc53674a1af023edafc35
vsem <virtualik@gmail.com>
2023-06-13 02:42:52 +0200

    serializers added

--- core/serializers.py
+++ core/serializers.py
@@ -13,3 +13,27 @@ class UserPublicSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ["id", "email", "first_name", "last_name"]
+
+
+class UserCreateRequestSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ["email", "password"]
+
+
+class UserCreateResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "email", "first_name", "last_name", "role"]
+
+
+class LoginRequestSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=150)
+    password = serializers.CharField(max_length=128)
+
+
+class LoginResponseSerializer(serializers.Serializer):
+    token = serializers.CharField(max_length=255)
