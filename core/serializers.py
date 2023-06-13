24f5f3689fa16f514d27e2e2775426e38c8cfafa
vsem <virtualik@gmail.com>
2023-06-14 01:32:58 +0200

    DRF serializer validation

--- core/serializers.py
+++ core/serializers.py
@@ -1,33 +1,27 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
+
+from core.constants import Role
 
 User = get_user_model()
 
 
-class UserCreateSerializer(serializers.Serializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
-
-
-class UserPublicSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "email", "first_name", "last_name"]
-
-
-class UserCreateRequestSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField()
-    password = serializers.CharField()
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(required=True, write_only=True)
+    first_name = serializers.CharField(required=False, read_only=True)
+    last_name = serializers.CharField(required=False, read_only=True)
+    role = serializers.IntegerField(required=False, read_only=True)
 
     class Meta:
         model = User
-        fields = ["email", "password"]
+        fields = ["id", "email", "password", "first_name", "last_name", "role"]
 
+    def validate(self, attrs):
+        attrs["password"] = make_password(attrs["password"])
+        attrs["role"] = Role.USER
 
-class UserCreateResponseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "email", "first_name", "last_name", "role"]
+        return attrs
 
 
 class LoginRequestSerializer(serializers.Serializer):
