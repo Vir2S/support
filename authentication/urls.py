1cd5bf6a90facb5454cd3e281b92e3d269c603ad
vsem <virtualik@gmail.com>
2023-06-14 16:00:57 +0200

    isort black codestyle fixed

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view()),
