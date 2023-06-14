602e842f081e03f072b45cf1f26516269a0ae9de
vsem <virtualik@gmail.com>
2023-06-14 15:47:22 +0200

    black codestyle fixed

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,5 +3,5 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view()),
-    path("token/refresh/", TokenRefreshView.as_view())
+    path("token/refresh/", TokenRefreshView.as_view()),
 ]
