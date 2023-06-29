bed05ba5f91cc641365161be2a10265c4f279efa
vsem <virtualik@gmail.com>
2023-06-30 00:27:01 +0200

    base url fixed

--- src/config/urls.py
+++ src/config/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("auth/", include("authentication.urls")),
+    path("api/auth/", include("authentication.urls")),
     path("api/pokemons/", include("pokemons.urls")),
     path("api/users/", include("users.urls")),
     path("api/users/create-random-user/", include("examples.users")),
