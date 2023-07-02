e01316fa026eee607fa1c2653cd9dcd029c3d8bb
vsem <virtualik@gmail.com>
2023-07-03 00:05:59 +0200

    path in swagger fixed

--- src/config/urls.py
+++ src/config/urls.py
@@ -21,7 +21,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("admin/", admin.site.urls),
     path(
-        "swagger/",
+        "docs/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
