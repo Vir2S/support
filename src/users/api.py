47a70fb9ab3f055ff86560399b5c17354a6bd56c
vsem <virtualik@gmail.com>
2023-07-18 22:43:34 +0200

    permissions for registration fixed

--- src/users/api.py
+++ src/users/api.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
 from users.serializers import UserRegistrationSerializer
 
 
 class UserRegistrationAPIView(CreateAPIView):
     serializer_class = UserRegistrationSerializer
+    permission_classes = (AllowAny, )
